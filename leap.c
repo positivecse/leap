5860389d04415ccb9d56a67315151d3f1f6dc6b5
positivecse <47621096+positivecse@users.noreply.github.com>
2019-08-11 17:01:17 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -3,17 +3,17 @@
 void main()
 {
     int year;
-
+   clrscr();
     printf("Enter a year: ");
     scanf("%d",&year);
 
-    if(year%4 == 0)
-printf("%d is a leap year:",year);
+        if(year%4 == 0)
+                 printf("%d is a leap year:",year);
        else if( year%100 == 0)
-        printf("%d is not a leap year.", year);
-     if ( year%400 == 0)
+                 printf("%d is not a leap year.", year);
+        else if ( year%400 == 0)
                 printf("%d is a leap year.", year);      
-    else
-        printf("%d is not a leap year.", year);
+        else
+                 printf("%d is not a leap year.", year);
     getch();
 }
